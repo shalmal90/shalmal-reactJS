37c630b2e85554590e43225976f8ce728b922849
Shalmal Sanjay Sheth <shalmal.sheth@gmail.com>
2020-03-06 13:29:17 -0500

    statechange

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//test change
-function Square(props){
-      return (
-        <button className="square" onClick={() => this.props.onClick()}>
-          {this.props.value}
-        </button>
-      );
+function Square(props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
   }
   
   class Board extends React.Component {
@@ -15,13 +14,17 @@ function Square(props){
       super(props);
       this.state = {
         squares: Array(9).fill(null),
+        xIsNext:true
       };
     }
   
     handleClick(i) {
       const squares = this.state.squares.slice();
-      squares[i] = 'X';
-      this.setState({squares: squares});
+      squares[i] = this.state.xIsNext?'X':'O';
+      this.setState({
+          squares: squares,
+          xIsNext:!this.state.xIsNext
+    });
     }
   
     renderSquare(i) {
@@ -40,13 +43,19 @@ function Square(props){
         <div>
           <div className="status">{status}</div>
           <div className="board-row">
-            {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
           </div>
           <div className="board-row">
-            {this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
           </div>
           <div className="board-row">
-            {this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
           </div>
         </div>
       );
